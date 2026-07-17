42374d3c77e59fa6a336eb7f2396a12bce5dbf1c
Jaime <jaimelabana@gmail.com>
2026-07-17 23:40:00 +0800

    Added some empty entities

---
+++ LMS.Domain/Entities/Courses/VideoLesson.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.Domain.Entities.Courses
+{
+    public class VideoLesson
+    {
+    }
+}
---
+++ LMS.Domain/Entities/Identity/Role.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LMS.Domain.Entities.Identity
+{
+    public class Role
+    {
+    }
+}
