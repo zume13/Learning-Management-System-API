6b13997a6269559fc632a4486a646c26ac107673
Dustine <dustinesarmiento3@gmail.com>
2026-07-18 18:21:23 +0800

    RefreshToken

--- LMS.Domain/Entities/Identity/Users/RefreshToken.cs
+++ LMS.Domain/Entities/Identity/Users/RefreshToken.cs
@@ -1,7 +1,34 @@
 ﻿
+using LMS.Domain.Primitives;
+
 namespace LMS.Domain.Entities.Identity.Users
 {
-    public class RefreshToken
+    public class RefreshToken : Entity
     {
+       public RefreshToken(Guid id, string tokenHash, Guid userId, DateTime expiryDate)
+            : base(id)
+        {
+            TokenHash = tokenHash;
+            UserId = userId;
+            ExpiryDate = expiryDate;
+        }
+
+        public string TokenHash { get; private set; }
+
+        public Guid UserId { get; private set; }
+
+        public DateTime ExpiryDate { get; private set; }
+
+        public DateTime CreateDate { get; private set; }
+
+        public bool Revoked { get; private set; }
+
+        public DateTime? RevokedDate { get; private set; }
+
+        public Guid? ReplacedByTokenID { get; private set; }
+
+
+        
+
     }
 }
