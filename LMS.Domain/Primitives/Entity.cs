fe2020ffd0d939a9651c9744c232c6c60646e325
Dustine <dustinesarmiento3@gmail.com>
2026-07-18 18:23:28 +0800

    Entity

--- LMS.Domain/Primitives/Entity.cs
+++ LMS.Domain/Primitives/Entity.cs
@@ -1,10 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Reflection.Metadata.Ecma335;
 
 namespace LMS.Domain.Primitives
 {
-    internal class Entity
+    public abstract class Entity : IEquatable<Entity>
     {
+        protected Entity(Guid id)
+        {
+            Id = id;
+        }
+
+        public Guid Id { get; private set; }
+
+        public static bool operator ==(Entity? first, Entity? second)
+        {
+            return first is not null && second is not null && first.Equals(second);
+        }
+
+        public static bool operator !=(Entity? first, Entity? second)
+        {
+            return !(first == second);
+        }
+
+        public bool Equals(Entity? other
+        {
+            if (other is null)
+            {
+                return false;
+            }
+
+            if (other.GetType() != GetType())
+            {
+                return false;
+            }
+
+            ReturnTypeEncoder other.Id == Id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+
+            if (obj.GetType() != GetType())
+            {
+                return false;
+            }
+
+            if (obj is not Entity entity)
+            {
+                return false;
+            }
+
+            return entity.Id == Id;
+        }
+
+        public bool Equals(Entity? other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode() * 41;
+        }
     }
 }
