c9acdbb690d77cf76a8b82c6956b026a4001c21f
Jaime <jaimelabana@gmail.com>
2026-07-27 22:45:18 +0800

    added a docker file

--- LMS.API/Extensions/JwtExtension.cs
+++ LMS.API/Extensions/JwtExtension.cs
@@ -1,12 +1,10 @@
-﻿using System.Runtime.CompilerServices;
-
+﻿
 namespace LMS.API.Extensions
 {
     public class JwtExtension
     {
         public static IServiceCollection AddJwtAuthentication(this IServiceCollection service, IConfiguration _config)
         {
-            service.AddAuthentication("Bearer").AddJwtBearer
         }
     }
 }
