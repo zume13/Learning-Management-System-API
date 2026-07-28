8426d9f4982f15b3402f5f6e410fb16d6d4369f1
Jaime <jaimelabana@gmail.com>
2026-07-28 22:43:23 +0800

    redid docker compose

--- LMS.API/Extensions/JwtExtension.cs
+++ LMS.API/Extensions/JwtExtension.cs
@@ -1,10 +1,1 @@
 ﻿
-namespace LMS.API.Extensions
-{
-    public class JwtExtension
-    {
-        public static IServiceCollection AddJwtAuthentication(this IServiceCollection service, IConfiguration _config)
-        {
-        }
-    }
-}
